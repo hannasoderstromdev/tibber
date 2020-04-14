bf8408a13ec679e878ddbe41247d20c521ca50b8
Hanna Söderström <hanna.soderstrom@netigate.se>
2020-04-14 08:26:29 +0200

    feat: render chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import "./App.css";
+
+export function transformDataForHighcharts(data) {
+  const temperatures = data.entries.map(({ temperature }) => {
+    return temperature;
+  });
+
+  const startTime = new Date(data.entries[0].time).getTime();
+  const endTime = new Date(
+    data.entries[data.entries.length - 1].time
+  ).getTime();
+
+  return {
+    temperatures,
+    startTime,
+    endTime,
+    maxTemperature: data.maxTemperature,
+    minTemperature: data.minTemperature,
+  };
+}
+
+function show({ target }) {
+  const series = target.xAxis.series;
+  let i = target.xAxis.length;
+  let otherSeries;
+
+  while (i--) {
+    otherSeries = series[i];
+    if (otherSeries !== target.xAxis.series && otherSeries.visible) {
+      otherSeries.hide();
+    }
+  }
+}
+
+function legendItemClick({ target }) {
+  if (target.visible) {
+    return false;
+  }
+}
+
+function formatYaxisLabels(thing) {
+  console.log(thing);
+  return "";
+}
 
 function App() {
+  const [hasError, setError] = useState(false);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch("http://localhost:3000/data");
+        const json = await res.json();
+        const transformedData = transformDataForHighcharts(json);
+        setData(transformedData);
+      } catch (error) {
+        setError(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+  const date = Date.UTC(2018, 2, 21);
+  const options = {
+    credits: {
+      enabled: false,
+    },
+    title: { text: "" },
+    series: [
+      {
+        name: "Temperature",
+        data: data.temperatures,
+        pointStart: data.startTime,
+        pointInterval: 1000 * 60 * 60,
+        type: "spline",
+        tooltip: {
+          valueDecimals: 1,
+        },
+        events: {
+          show: show,
+          legendItemClick: legendItemClick,
+        },
+      },
+    ],
+    xAxis: {
+      type: "datetime",
+      tickInterval: 1000 * 60 * 60,
+      title: { text: "" },
+    },
+    yAxis: {
+      type: "linear",
+      min: data.minTemperature,
+      max: data.maxTemperature,
+      title: { text: "" },
+      labels: {
+        format: `{value}C`,
+      },
+    },
+    legend: {
+      enabled: false,
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
   );
 }
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,72 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App, { transformDataForHighcharts } from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("transformDataForHighcharts", () => {
+  it("returns data in correct format", () => {
+    const data = {
+      minTemperature: 5.2,
+      maxTemperature: 10.5,
+      entries: [
+        { time: "2020-04-09T00:00:00+02:00", temperature: 9.6, type: "cloud" },
+        { time: "2020-04-09T01:00:00+02:00", temperature: 9.4, type: "cloud" },
+        { time: "2020-04-09T02:00:00+02:00", temperature: 9, type: "cloud" },
+        { time: "2020-04-09T03:00:00+02:00", temperature: 8.5, type: "cloud" },
+        { time: "2020-04-09T04:00:00+02:00", temperature: 8.6, type: "cloud" },
+        { time: "2020-04-09T05:00:00+02:00", temperature: 7.1, type: "cloud" },
+        { time: "2020-04-09T06:00:00+02:00", temperature: 6.4, type: "cloud" },
+        { time: "2020-04-09T07:00:00+02:00", temperature: 6, type: "sun" },
+        { time: "2020-04-09T08:00:00+02:00", temperature: 6.4, type: "sun" },
+        { time: "2020-04-09T09:00:00+02:00", temperature: 6.7, type: "sun" },
+        { time: "2020-04-09T10:00:00+02:00", temperature: 7.5, type: "sun" },
+        { time: "2020-04-09T11:00:00+02:00", temperature: 8.7, type: "sun" },
+        { time: "2020-04-09T12:00:00+02:00", temperature: 9, type: "cloud" },
+        { time: "2020-04-09T13:00:00+02:00", temperature: 9.6, type: "cloud" },
+        { time: "2020-04-09T14:00:00+02:00", temperature: 10, type: "cloud" },
+        { time: "2020-04-09T15:00:00+02:00", temperature: 10.4, type: "cloud" },
+        { time: "2020-04-09T16:00:00+02:00", temperature: 10.5, type: "cloud" },
+        { time: "2020-04-09T17:00:00+02:00", temperature: 10.5, type: "cloud" },
+        { time: "2020-04-09T18:00:00+02:00", temperature: 9.6, type: "cloud" },
+        { time: "2020-04-09T19:00:00+02:00", temperature: 9, type: "sun" },
+        { time: "2020-04-09T20:00:00+02:00", temperature: 8, type: "sun" },
+        { time: "2020-04-09T21:00:00+02:00", temperature: 6.7, type: "sun" },
+        { time: "2020-04-09T22:00:00+02:00", temperature: 5.9, type: "sun" },
+        { time: "2020-04-09T23:00:00+02:00", temperature: 5.2, type: "sun" },
+      ],
+    };
+    const result = transformDataForHighcharts(data);
+    const expected = {
+      minTemperature: 5.2,
+      maxTemperature: 10.5,
+      endTime: 1586466000000,
+      startTime: 1586383200000,
+      temperatures: [
+        9.6,
+        9.4,
+        9,
+        8.5,
+        8.6,
+        7.1,
+        6.4,
+        6,
+        6.4,
+        6.7,
+        7.5,
+        8.7,
+        9,
+        9.6,
+        10,
+        10.4,
+        10.5,
+        10.5,
+        9.6,
+        9,
+        8,
+        6.7,
+        5.9,
+        5.2,
+      ],
+    };
+    expect(result).toEqual(expected);
+  });
 });
